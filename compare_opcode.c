5c216e313bf792373dfe451b4a03ecbcdab08734
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-05-13 22:07:25 -0500

    compare array words with opcode functions

---
+++ compare_opcode.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+* compare_execution - compare array words with opcode functions 
+*
+* Return: void
+*/
+void *compare_execution(char **array_words, stack_t **head, unsigned int n_line)
+{
+	instruction_t opcode_functions[] = {{"push", _push}, {"pall", _pall}, {NULL, NULL}};
+	unsigned int i;
+
+	/* comparar array words con opcode functions */
+	while (opcode_functions[i].opcode != NULL)
+	{
+		if(strcmp(opcode_functions[i].opcode, array_words[0]) == 0)
+		{
+			printf("FUNCION COMPARADA, DENTRO DE COMPARE_EXECUTION: %s\n", opcode_functions[i].opcode);
+			opcode_functions[i].f(head, n_line );
+		}
+		i++;
+	}
